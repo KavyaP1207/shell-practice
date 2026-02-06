20f6a0d91a05b842e72ff3d03bfccf95a94cdf2a
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-06 22:39:51 +0530

    helloworld

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-PERSON1="john"
-PERSON2="Ruhi"
-echo "$PERSON1 :: hai $PERSON2 how are you "
-echo "$PERSON2:: hello $PERSON1, im gud h=what about you "
-echo "$PERSON1 :: what you are learning recently"
-echo "$PERSON2:: devops"
+
+echo "ramesh :: hai suresh how are you "
+echo "suresh:: hello ramesh, im gud h=what about you "
+echo "ramesh :: what you are learning recently"
+echo "suresh:: devops"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1="john"
+PERSON2="Ruhi"
+echo "$PERSON1 :: hai $PERSON2 how are you "
+echo "$PERSON2:: hello $PERSON1, im gud h=what about you "
+echo "$PERSON1 :: what you are learning recently"
+echo "$PERSON2:: devops"
