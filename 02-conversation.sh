54bf9ffb6da7495653167edfd7e3638844a7df68
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-06 22:37:51 +0530

    helloworld

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "ramesh :: hai suresh how are you "
-echo "suresh:: hello ramesh, im gud h=what about you "
-echo "ramesh :: what you are learning recently"
-echo "suresh:: devops"
+PERSON1="john"
+PERSON2="Ruhi"
+echo "$PERSON1 :: hai $PERSON2 how are you "
+echo "$PERSON2:: hello $PERSON1, im gud h=what about you "
+echo "$PERSON1 :: what you are learning recently"
+echo "$PERSON2:: devops"
