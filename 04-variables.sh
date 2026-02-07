44077d4afaa36484b57f76d412f16a1b0ba35850
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-07 13:22:11 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 :: hai $PERSON2 how are you "
+echo "$PERSON2:: hello $PERSON1, im gud h=what about you "
+echo "$PERSON1 :: what you are learning recently"
+echo "$PERSON2:: devops"
