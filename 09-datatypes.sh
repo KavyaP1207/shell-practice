4c5df7fea363fd791718928ad1cc153d69992c3a
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-07 17:04:01 +0530

    variables

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -8,3 +8,10 @@ number=200
 sum=$(($number1+$number))
 
 echo "sum is : $sum"
+
+#size =4, max index =3
+
+leaders=("kavya" "prema" "shekhar")
+
+echo "all leaders : ${leaders[@]}"
+echo "first leader : ${leader[0]}"
