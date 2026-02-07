cc12b9679f4dff24dbe1df9d5ba3e972eead8198
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-07 16:44:13 +0530

    variables

--- 08-splvar.sh
+++ 08-splvar.sh
@@ -3,3 +3,6 @@
 echo "all variables passed to the script: $@"
 echo "all variables passed to the script:$*"
 echo "script name : $0"
+echo "current directory : $PWD"
+echo "who is running tis : $USER"
+echo "home directory of user : $HOME"
