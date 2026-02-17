83c9c16d37454e5da0c86513370497319b06fafb
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-17 20:50:34 +0530

    helloworld

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
    echo "Error: : please run this script with root privelage"
+   exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
   echo "ERROR: : installing mysql is failure"
+  exit 1
 else
   echo "Installing mysql is success"
 fi
