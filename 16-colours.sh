3339471f10d0cf863bfb7e717ec826226ec6c7fc
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-17 22:08:45 +0530

    helloworld

--- 16-colours.sh
+++ 16-colours.sh
@@ -25,14 +25,14 @@ if [ $1 -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? "mysql"
 else 
-    echo -e "Mysql is already exists $G skipping $Y"
+    echo -e "Mysql is already exists $Y skipping $N"
 fi
 dnf list installed nginx
 if [ $1 -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "nginx"
 else 
-    echo -e "nginx is already exists $G skipping $Y"
+    echo -e "nginx is already exists $Y skipping $N"
 fi
 
 dnf list installed python3
@@ -40,5 +40,5 @@ if [ $1 -ne 0 ]; then
     dnf install python3 -y
     VALIDATE $? "python3"
 else 
-    echo -e "python3 is already exists $G skipping $Y"
+    echo -e "python3 is already exists $Y skipping $N"
 fi
