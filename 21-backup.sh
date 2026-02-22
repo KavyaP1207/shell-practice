8398cd016ecf51c776c32f3daf91cbc6ac686d12
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-22 22:49:23 +0530

    sessions

--- 21-backup.sh
+++ 21-backup.sh
@@ -9,7 +9,7 @@ Y="\e[33m"
 N="\e[0m"
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=$(3: -14)
+#DAYS=$(3: -14)
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -44,7 +44,7 @@ FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 
 if [ ! -z "${FILES}" ]; then 
    echo "Files found"
-   
+
 else 
     echo " no files to archive"
 fi
